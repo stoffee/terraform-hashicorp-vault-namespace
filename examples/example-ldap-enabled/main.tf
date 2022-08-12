bf3827190ab845f742091411f03c9c04c88449c9
stoffee <chris@roadkill.org>
2022-08-12 13:09:37 -0700

    added ad secrets

--- examples/example-ldap-enabled/main.tf
+++ examples/example-ldap-enabled/main.tf
@@ -10,6 +10,26 @@ module "vault-namespace" {
   #
   kv2_enabled = true
 
+
+  #
+  # LDAP Auth Engine
+  #
+  ldap_auth_enabled         = true
+  ldap_auth_engine_ldap_url = var.ldap_auth_engine_ldap_url
+  ldap_auth_engine_bindpass = var.ldap_auth_engine_bindpass
+  ldap_auth_userdn          = var.ldap_auth_userdn
+  ldap_auth_userattr        = var.ldap_auth_userattr
+  ldap_auth_groupattr       = var.ldap_auth_groupattr
+  ldap_auth_groupdn         = var.ldap_auth_groupdn
+  ldap_auth_groupfilter     = var.ldap_auth_groupfilter
+  ldap_auth_binddn          = var.ldap_auth_binddn
+  ldap_auth_insecure_tls    = var.ldap_auth_insecure_tls
+  ldap_auth_starttls        = var.ldap_auth_starttls
+
+  #
+  # Active Directory Secrets
+  #
+  ad_secrets_enabled = true
   #
   # AppRole Auth
   #
@@ -69,20 +89,4 @@ module "vault-namespace" {
   #
   gcp_secrets_enabled        = false
   gcp_secrets_engine_project = "false"
-
-  #
-  # LDAP Auth Engine
-  #
-  ldap_auth_enabled         = true
-  ldap_auth_engine_ldap_url = var.ldap_auth_engine_ldap_url
-  ldap_auth_engine_bindpass = var.ldap_auth_engine_bindpass
-  ldap_auth_userdn          = var.ldap_auth_userdn
-  ldap_auth_userattr        = var.ldap_auth_userattr
-  ldap_auth_groupattr       = var.ldap_auth_groupattr
-  ldap_auth_groupdn         = var.ldap_auth_groupdn
-  ldap_auth_groupfilter     = var.ldap_auth_groupfilter
-  ldap_auth_binddn          = var.ldap_auth_binddn
-  ldap_auth_insecure_tls    = var.ldap_auth_insecure_tls
-  ldap_auth_starttls        = var.ldap_auth_starttls
-
 }
