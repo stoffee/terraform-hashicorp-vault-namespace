06405d26637826327839b593d5987d7012205c5f
stoffee <chris@roadkill.org>
2022-10-17 13:09:10 -0700

    changed outputs

--- outputs.tf
+++ outputs.tf
@@ -2,11 +2,11 @@ output "namespace" {
   description = "The namespace path"
   value       = vault_namespace.new.path
 }
-output "vault_ldap_auth_backend_accessor" {
+output "vault_ldap_auth_backend_mount_accessor" {
   description = "The ldap auth backend accessor"
   value       = vault_ldap_auth_backend.ldap[0].accessor
 }
-output "vault_admin_identity_group_cannonical_id" {
+output "vault_admin_identity_group_canonical_id" {
   description = "The ldap auth backend accessor"
   value       = vault_identity_group.vault_admin.id
 }
