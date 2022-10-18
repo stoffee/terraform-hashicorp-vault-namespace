09587e1d06b1aa8b5420ef954e275434b30ad776
stoffee <chris@roadkill.org>
2022-10-18 13:23:59 -0700

    enhanced exmaples-ldap-kv2

--- ldap_admin_identity_group.tf
+++ ldap_admin_identity_group.tf
@@ -1,6 +1,6 @@
 #group name
 resource "vault_identity_group" "vault_admin" {
-  name      = var.vault_admin_group
+  name      = var.ldap_vault_admin_group
   type      = "external"
   policies  = ["default", "super-user-pol"]
   namespace = vault_namespace.new.path
@@ -9,7 +9,7 @@ resource "vault_identity_group" "vault_admin" {
 #group alias for ldap
 resource "vault_identity_group_alias" "vault_admin_alias" {
   count          = var.ldap_auth_enabled ? 1 : 0
-  name           = var.vault_admin_group
+  name           = var.ldap_vault_admin_group
   mount_accessor = vault_ldap_auth_backend.ldap[0].accessor
   canonical_id   = vault_identity_group.vault_admin.id
 }
